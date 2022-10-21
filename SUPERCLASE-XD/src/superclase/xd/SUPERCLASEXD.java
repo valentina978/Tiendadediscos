23b7db872f8fa26b280c3443c115c2230f5b3993
valentina978 <114037877+valentina978@users.noreply.github.com>
2022-10-20 19:02:39 -0500

    vale

--- SUPERCLASE-XD/src/superclase/xd/SUPERCLASEXD.java
+++ SUPERCLASE-XD/src/superclase/xd/SUPERCLASEXD.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package superclase.xd;
 
-/**
- *
- * @author cante
- */
 public class SUPERCLASEXD {
 
-    /**
-     * @param args the command line arguments
-     */
      private String genero;
     
       private String autor;
